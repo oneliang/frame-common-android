d521dc6da8dea2944efc1545b72d70d0a15f82d4
oneliang <oneliang@tencent.com>
2017-11-29 16:39:30 +0800

    optimize code

--- src/main/java/com/oneliang/android/common/broadcast/BroadcastManager.java
+++ src/main/java/com/oneliang/android/common/broadcast/BroadcastManager.java
@@ -1,36 +1,42 @@
 package com.oneliang.android.common.broadcast;
 
 import android.os.Handler;
+
+import com.oneliang.Constant;
 import com.oneliang.frame.broadcast.Message;
+import com.oneliang.util.logging.Logger;
+import com.oneliang.util.logging.LoggerManager;
 
-public final class BroadcastManager extends com.oneliang.frame.broadcast.BroadcastManager{
+public final class BroadcastManager extends com.oneliang.frame.broadcast.BroadcastManager {
 
-    private Handler handler=new Handler(){
+    private static final Logger logger = LoggerManager.getLogger(BroadcastManager.class);
+
+    private Handler handler = new Handler() {
         public void handleMessage(android.os.Message osMessage) {
-            if(osMessage.obj!=null&&osMessage.obj instanceof Message){
-                Message message=(Message)osMessage.obj;
+            if (osMessage.obj != null && osMessage.obj instanceof Message) {
+                Message message = (Message) osMessage.obj;
                 BroadcastManager.this.handleMessage(message);
             }
         };
     };
 
     public void run() {
-        while(!Thread.currentThread().isInterrupted()){
-        	try{
-                if(!this.messageQueue.isEmpty()){
-                    Message message=this.messageQueue.poll();
-                    android.os.Message osMessage=android.os.Message.obtain();
-                    osMessage.obj=message;
+        try {
+            while (!Thread.currentThread().isInterrupted()) {
+                if (!this.messageQueue.isEmpty()) {
+                    Message message = this.messageQueue.poll();
+                    android.os.Message osMessage = android.os.Message.obtain();
+                    osMessage.obj = message;
                     handler.sendMessage(osMessage);
-                }else{
+                } else {
                     synchronized (this) {
                         this.wait();
                     }
                 }
-	            
-	        }catch (Exception e) {
-	            e.printStackTrace();
-	        }
-    	}
+            }
+        } catch (Exception e) {
+            logger.error(Constant.Base.EXCEPTION, e);
+            Thread.currentThread().interrupt();
+        }
     }
 }
